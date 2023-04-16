1e72e87deea90d21c7ee180542477b4ee1faf5cf
ali19 <ali19158@gmail.com>
2023-04-16 12:47:32 +0600

    space after add text

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 const (
 	chooseAction       = "Write action (buy, fill, take, remaining, exit): "
 	buyChoice          = "What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, 4 - back:"
-	fillWaterQuestion  = "Write how many ml of water you want to add:"
+	fillWaterQuestion  = "Write how many ml of water you want to add: "
 	fillMilkQuestion   = "Write how many ml of milk you want to add: "
 	fillCoffeeQuestion = "Write how many grams of coffee beans you want to add: "
 	fillCupsQuestion   = "Write how many disposable cups you want to add: "
